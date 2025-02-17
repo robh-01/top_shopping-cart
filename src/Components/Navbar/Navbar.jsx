1a9a091ba3538ff14d912d2823537f0c9ebe8c31
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-02-17 22:12:03 +0545

    Structure navbar with router links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,35 @@
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  return <div>
-    <Link to="/">Home</Link>
-    <Link to="/products">Products</Link>
-    <Link to="/about">About</Link>
-    <Link to="/cart">Cart</Link>
-  </div>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={"container " + styles["container--navbar"]}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link to="/about" className={styles.navLink}>
+              About
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link to="/products" className={styles.navLink}>
+              Products
+            </Link>
+          </li>
+        </ul>
+        <div className={styles.logo}>
+          <Link to="/" className={styles.navLink}>
+            Shop<span>Up</span>
+          </Link>
+        </div>
+        <div className={styles.cartIcon}>
+          <Link to="/cart" className={styles.navLink}>
+            <ShoppingCart />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
